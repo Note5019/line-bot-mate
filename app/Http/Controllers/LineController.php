44906c9356c5e0dfae82081faa4d99a74a8b040d
Noppadon Tongtrakoon <notenoppadon5019@gmail.com>
2024-01-03 22:51:44 +0700

    Edit line handle controller

--- app/Http/Controllers/LineController.php
+++ app/Http/Controllers/LineController.php
@@ -6,9 +6,10 @@ use Illuminate\Http\Request;
 
 class LineController extends Controller
 {
-    public function handle()
+    public function handle(Request $request)
     {
         \Log::info('called');
+        \Log::info(print_r($request->input(), true));
         // TODO: process word input
         return ['success' => true];
     }
---
+++ config/line.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'line_channel_access_token' => env('LINE_CHANNEL_ACCESS_TOKEN', ''),
+    'user_id' => env('USER_ID', ''),
+];
