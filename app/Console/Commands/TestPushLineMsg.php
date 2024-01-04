aedff3d8858c2ad8b9e5583b7de1ee87a7ec66c8
Noppadon Tongtrakoon <notenoppadon5019@gmail.com>
2024-01-04 20:58:33 +0700

    Test cmd

--- app/Console/Commands/TestPushLineMsg.php
+++ app/Console/Commands/TestPushLineMsg.php
@@ -7,6 +7,11 @@ use Illuminate\Support\Facades\Http;
 use App\Actions\PushLineMessage;
 use App\Actions\BuildGoldLineMessage;
 use App\Actions\FetchGoldPrice;
+use App\Actions\SaveGoldPrice;
+use App\Models\Gold;
+use App\Models\MyGold;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class TestPushLineMsg extends Command
 {
@@ -142,23 +147,44 @@ class TestPushLineMsg extends Command
   public function handle()
   {
     $data = FetchGoldPrice::execute();
-
-    // save gold price to DB
-
-    $gold = collect($data)->filter(function ($item) {
-      return $item['GoldType'] === 'HSH' && $item['GoldCode'] === '96.50';
+    $rawGold = collect($data)->filter(function ($item) {
+      return $item['GoldType'] === Gold::GOLD_TYPE && $item['GoldCode'] === Gold::GOLD_CODE;
     })->first();
 
-    dump($gold);
+    $gold = SaveGoldPrice::execute($rawGold);
 
-    $topic = "{$gold['GoldType']} {$gold['GoldCode']}%";
-    $buyPrice = $gold['Buy'];
-    $sellPrice = $gold['Sell'];
-    $date = $gold['StrTimeUpdate'];
-    $msg = BuildGoldLineMessage::execute($topic, $buyPrice, $sellPrice, $date);
-// dump(json_encode($msg));
+    // diff from myGold
+    // $this->findGoldCanSell();
+
+
+
+    // dump($gold);
+
+    // $topic = "{$gold['GoldType']} {$gold['GoldCode']}%";
+    // $buyPrice = $gold['Buy'];
+    // $sellPrice = $gold['Sell'];
+    // $date = $gold['StrTimeUpdate'];
+    // $msg = BuildGoldLineMessage::execute($topic, $buyPrice, $sellPrice, $date);
+    // dump(json_encode($msg));
     // $pusher = new PushLineMessage();
     // $altText = 'ราคาทองมาใหม่แล้ว';
     // $pusher->execute($altText, $msg);
   }
+
+  // public function findGoldCanSell()
+  // {
+  //   $today = Carbon::now()->startOfDay();
+  //   $currentGold = Gold::where('created_at', '>', $today)->latest()->first();
+  //   // above target_sell_price
+  //   $records = MyGold::whereNotNull('target_sell_price')->where('target_sell_price', '<=', $currentGold->buy)->get();
+  //   // should notify
+
+  //   // above target_baht_profit
+  //   $records = DB::table('my_gold')
+  //     ->whereRaw('? - buy_price > target_baht_profit', [$currentGold->buy])
+  //     ->get();
+  //     dump($currentGold->buy);
+  //     dump($records);
+  //     // should notify
+  // }
 }
