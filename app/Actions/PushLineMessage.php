908e21465cb20f7da9d553abd20baa8899992eea
Noppadon Tongtrakoon <notenoppadon5019@gmail.com>
2024-01-04 20:18:11 +0700

    Edit push ling msg action

--- app/Actions/PushLineMessage.php
+++ app/Actions/PushLineMessage.php
@@ -6,28 +6,30 @@ use Illuminate\Support\Facades\Http;
 
 class PushLineMessage
 {
-  public function execute(string $altText, array $msgPayload, string $to = null): void
+  public static function execute(array $msgPayload, string $to = null): void
   {
     $to = $to ?? config('line.user_id');
     $token = config('line.line_channel_access_token');
     $payload = [
       'to' => $to,
-      'messages' => [
-        [
-          "type" => "flex",
-          "altText" => "this is a flex message",
-          "contents" => $msgPayload,
-        ]
-      ]
+      // 'messages' => [
+      //   [
+      //     "type" => "flex",
+      //     "altText" => $altText,
+      //     "contents" => $msgPayload,
+      //   ]
+      // ]
+      'messages' => $msgPayload,
     ];
 
     $response = Http::withHeaders([
       'Content-Type' => 'application/json',
       'Authorization' => "Bearer {$token}"
     ])->post('https://api.line.me/v2/bot/message/push', $payload);
-    dump($response->status());
+
     if ($response->status() > 200) {
       \Log::error($response->json());
+      // TODO: line noti error
     }
   }
 }
