b3fd33cc5aac2cd26fed0ac36b80ad918ff1f134
Noppadon Tongtrakoon <notenoppadon5019@gmail.com>
2024-01-04 23:21:10 +0700

    Add line controller handle

--- app/Http/Controllers/LineController.php
+++ app/Http/Controllers/LineController.php
@@ -2,15 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\HandleLineMsg;
+use App\Actions\NotifyError;
+use App\Enums\ResponseCode;
 use Illuminate\Http\Request;
 
 class LineController extends Controller
 {
     public function handle(Request $request)
     {
-        \Log::info('called');
-        \Log::info(print_r($request->input(), true));
-        // TODO: process word input
+        $handler = new HandleLineMsg($request->input());
+        $res = $handler->execute();
+
+        if ($res->code > ResponseCode::OK) {
+            NotifyError::execute($res->topic, $res->message);
+        } else {
+            // TODO: success notify
+        }
+
         return ['success' => true];
     }
 }
