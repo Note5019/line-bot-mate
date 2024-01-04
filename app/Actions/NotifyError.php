42a75f4975111e70717d4a4135fa08c33fac1456
Noppadon Tongtrakoon <notenoppadon5019@gmail.com>
2024-01-04 22:43:56 +0700

    Add noti error

---
+++ app/Actions/NotifyError.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Actions;
+
+use App\Actions\PushLineMessage;
+
+class NotifyError
+{
+  public static function execute(string $topic, string $message): void
+  {
+    $msgPayload = [
+      "type" => "flex",
+      "altText" => $topic,
+      "contents" => self::message($topic, $message),
+    ];
+
+    PushLineMessage::execute($msgPayload);
+    \Log::info('push msg!');
+  }
+
+  private static function message(string $topic, string $message): array
+  {
+    $json = '{"type":"bubble","header":{"type":"box","layout":"vertical","contents":[{"type":"text","text":"##topic","color":"#FCF5ED"}]},"body":{"type":"box","layout":"vertical","contents":[{"type":"text","text":"##error","color":"#FCF5ED"}]},"styles":{"header":{"separatorColor":"#BF3131","separator":true,"backgroundColor":"#7D0A0A"},"body":{"backgroundColor":"#CE5A67"}}}';
+    $json = str_replace('##topic', $topic, $json);
+    $json = str_replace('##error', $message, $json);
+
+    return json_decode($json, true);
+  }
+}
---
+++ app/Console/Commands/TestNoti.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Actions\NotifyError;
+use Illuminate\Console\Command;
+
+class TestNoti extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'noti:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Test send noti';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $topic = 'หัวเรื่อง';
+        $message = 'ข้อความ';
+        NotifyError::execute($topic, $message);
+    }
+}
